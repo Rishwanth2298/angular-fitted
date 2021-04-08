38028e6acf5237ea4426725fb1ccdfeb77acdcd4
unknown <rishwanths2298a@gmail.com>
2021-04-08 06:32:15 +0100

    brag-tech

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const nodemailer = require("nodemailer");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors({origin: "*"}));
+
+app.get('/', function(_req,res){
+    res.send('Hello from server');
+})
+
+app.post('/enroll', async(req, res) => {
+    const {email}= req.body;
+    const {phone}= req.body;
+    const {name}= req.body;
+    const {message}= req.body;
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'rishwanths2298a@gmail.com',
+          pass: 'suji2298'
+        },
+      });
+    
+      const msg ={
+        
+            from:  '"Brag tech" <rishwanths2298a@gmail.com>', // sender address
+            to: 'rishwanths2298a@gmail.com', // list of receivers
+          
+            subject: "Angular Bragtech Form", // Subject line
+            text: "Contact Form ", // plain text body
+            html: `Name: ${name} , Mail id: ${email} , Mobile Number: ${phone} , Message: ${message}`,
+           
+             // html body
+      }
+      // send mail with defined transport object
+      const info = await transporter.sendMail(msg);
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+
+    res.status(200).send({"message": "Data received"});
+})
+
+app.listen(PORT, function(){
+    console.log("Server running on localhost:" + PORT);
+});
+
+
