bbdf52a12e6aa711dad8128d15db5c89ca012378
Rishwanth2298 <74642733+Rishwanth2298@users.noreply.github.com>
2021-11-16 09:00:29 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(bodyParser.json());
 
-
+app.use(cors({origin:"*"}));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://bragtech.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
