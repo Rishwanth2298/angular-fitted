5e05fc6167f0df1358e8b0d26a46bdd539088af9
Rishwanth2298 <74642733+Rishwanth2298@users.noreply.github.com>
2021-11-16 08:39:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(bodyParser.json());
 
-app.use(cors({origin:"*"}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
