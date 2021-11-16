d4ce50a6713548848292711152c49529054399a0
Rishwanth2298 <74642733+Rishwanth2298@users.noreply.github.com>
2021-11-16 06:30:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require("nodemailer");
 const app = express();
-
 app.use(bodyParser.json());
 
 app.use(cors({origin:"*"}));
